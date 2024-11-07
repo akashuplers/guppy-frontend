a712a69534feab7cf41cffcade7bee2be6608773
afreen-md <afreen@digitalsusa.com>
2024-11-07 23:36:37 +0530

    fix: added actions in compare json page

--- src/components/compare-jsons/JsonBody.jsx
+++ src/components/compare-jsons/JsonBody.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
 
 const JsonBody = (jsonData) => {
   const data = JSON.parse(jsonData.data);
+
+  const [situationData, setSituationData] = useState([]);
+  const [actionData, setActionData] = useState([]);
+
+  const filterSituationsAndActions = () => {
+    let situations = [];
+    let actions = [];
+    data.Ideas.updatedIdeas.forEach((ideaItem) => {
+      if(ideaItem.Classification === "Action")
+        actions.push(ideaItem);
+      else
+        situations.push(ideaItem);
+    })
+    setSituationData(situations);
+    setActionData(actions);
+  }
+
+  useState(() => {
+    filterSituationsAndActions();
+  },[])
+
   return (
     <div style={{ minWidth: '50vh', overflowX: 'auto', backgroundColor: 'white' }}>
       <p className="font-medium mb-2">
@@ -45,10 +67,11 @@ const JsonBody = (jsonData) => {
           )): <div>No Items</div>}
         </ul>
       </div>
+
       <p className="mt-6 mb-3 font-medium">TITLES :</p>
       {data.Title.updatedTitles.length > 0 ? data.Title.updatedTitles.map((titleItem, index) => (
         <div key={index} className="flex-row ml-12 mb-2">
-          <p className="mb-2 font-medium">{`${index+1}. ${titleItem.title}`}</p>
+          <p className="mb-2 font-medium">{`${index+1}. ${titleItem.title ?? 'No Title'}`}</p>
           <div className="flex-row ml-12">
             <p>Who Primary:</p>
             <ul className="ps-8 ml-12 mb-2">
@@ -89,11 +112,12 @@ const JsonBody = (jsonData) => {
           </div>
         </div>
       )) : <div>No Titles</div>
-    }
+      }
+
       <p className="mt-6 mb-3 font-medium">SITUATIONS :</p>
-      {data.Ideas.updatedIdeas.length > 0 ? data.Ideas.updatedIdeas.map((ideaItem, index) => (
+      {situationData.length > 0 ? situationData.map((ideaItem, index) => (
       <>
-        <p className="mb-2 font-medium">Idea: {`${index + 1}. ${ideaItem.idea}`}</p>
+        <p className="mb-2 font-medium">Idea: {`${index + 1}. ${ideaItem.idea ?? 'No Idea'} `}</p>
         <div key={index} className="flex-row ml-12 mb-2">
           <div className="flex-row ml-12">
             <p>Who Primary:</p>
@@ -136,6 +160,54 @@ const JsonBody = (jsonData) => {
         </div>
       </>
       )) : <div>No Situations</div>
+      }
+
+      <p className="mt-6 mb-3 font-medium">ACTIONS :</p>
+      {actionData.length > 0 ? actionData.map((ideaItem, index) => (
+        <>
+          <p className="mb-2 font-medium">Idea: {`${index + 1}. ${ideaItem.idea ?? 'No Idea'}`}</p>
+          <div key={index} className="flex-row ml-12 mb-2">
+            <div className="flex-row ml-12">
+              <p>Who Primary:</p>
+              <ul className="ps-8 ml-12 mb-2">
+                {ideaItem.ws.Who_Primary.length > 0 ? ideaItem.ws.Who_Primary.map((item, idx) => (
+                  <li className="list-disc" key={idx}>{item}</li>
+                )) : <div>No Items</div>}
+              </ul>
+              <p>Who Secondary:</p>
+              <ul className="ps-8 ml-12 mb-2">
+                {ideaItem.ws.Who_Secondary.length > 0 ? ideaItem.ws.Who_Secondary.map((item, idx) => (
+                  <li className="list-disc" key={idx}>{item}</li>
+                )) : <div>No Items</div>}
+              </ul>
+              <p>What Primary:</p>
+              <ul className="ps-8 ml-12 mb-2">
+                {ideaItem.ws.What_Primary.length > 0 ? ideaItem.ws.What_Primary.map((item, idx) => (
+                  <li className="list-disc" key={idx}>{item}</li>
+                )) : <div>No Items</div>}
+              </ul>
+              <p>What Secondary:</p>
+              <ul className="ps-8 ml-12 mb-2">
+                {ideaItem.ws.What_Secondary.length > 0 ? ideaItem.ws.What_Secondary.map((item, idx) => (
+                  <li className="list-disc" key={idx}>{item}</li>
+                )) : <div>No Items</div>}
+              </ul>
+              <p>Where Primary:</p>
+              <ul className="ps-8 ml-12 mb-2">
+                {ideaItem.ws.Where_Primary.length > 0 ? ideaItem.ws.Where_Primary.map((item, idx) => (
+                  <li className="list-disc" key={idx}>{item}</li>
+                )) : <div>No Items</div>}
+              </ul>
+              <p>Where Secondary:</p>
+              <ul className="ps-8 ml-12 mb-2">
+                {ideaItem.ws.Where_Secondary.length > 0 ? ideaItem.ws.Where_Secondary.map((item, idx) => (
+                  <li className="list-disc" key={idx}>{item}</li>
+                )) : <div>No Items</div>}
+              </ul>
+            </div>
+          </div>
+        </>
+        )) : <div>No Actions</div>
       }
     </div>
   );
