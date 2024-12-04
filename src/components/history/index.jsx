f6752ec6774b25b3f49c59bb19d457577569cf73
pragati-dev-hub <pragati.t@digitalsusa.com>
2024-12-05 00:39:28 +0530

    Update index.jsx

--- src/components/history/index.jsx
+++ src/components/history/index.jsx
@@ -296,7 +296,7 @@ const UserHistory = () => {
         <div>
             {/* head */}
             <p className="text-xl md:text-3xl mt-1 mb-2 md:mb-0 font-medium">User History</p>
-          
+
             {/* body */}
             <div className='mt-8'>
               {!isLoading &&
@@ -323,4 +323,3 @@ const UserHistory = () => {
 }
 
 export default UserHistory
-
